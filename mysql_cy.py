83428233653cb6ab83980c36eda52d120df891b9
Mark Stacy <mbstacy@gmail.com>
2014-06-09 08:47:45 -0500

    Set as function and add __main__

--- mysql_cy.py
+++ mysql_cy.py
@@ -5,21 +5,28 @@ from config import host,username,password
 from json_handler import handler
 import cymysql, json
 conn = cymysql.connect(host=host, user=username, passwd=password, db='srchdb', charset='utf8')
-cur = conn.cursor()
-
-#First query returning column names
-#Added to List
-columns=[]
-cur.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'srchdb' AND TABLE_NAME = 'dokarrs'")
-for r in cur.fetchall():
-    columns.append(r[0])
 
 
-#Retrieve Data
-data = []
-cur.execute('select * from dokarrs')
-for r in cur.fetchall():
-    #zip takes 2 list and creates tuples and dict to get dictionary
-    data.append(dict(zip(columns,list(r))))
+def get_json():
+    cur = conn.cursor()
 
-print json.dumps(data,default = handler,indent=4)
+    #First query returning column names
+    #Added to List
+    columns=[]
+    cur.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'srchdb' AND TABLE_NAME = 'dokarrs'")
+    for r in cur.fetchall():
+        columns.append(r[0])
+
+
+    #Retrieve Data
+    data = []
+    cur.execute('select * from dokarrs')
+    for r in cur.fetchall():
+        #zip takes 2 list and creates tuples and dict to get dictionary
+        data.append(dict(zip(columns,list(r))))
+    return json.dumps(data,default = handler,indent=4)
+
+
+if __name__ == "__main__":
+    
+    print get_json()
