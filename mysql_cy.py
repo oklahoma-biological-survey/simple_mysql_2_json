14d14151c3441ebdfe2772b7600c02940bf6d140
Mark Stacy <mbstacy@gmail.com>
2014-06-10 10:16:31 -0500

    Updated function to have keyword args for database and table

--- mysql_cy.py
+++ mysql_cy.py
@@ -4,23 +4,23 @@
 from config import host,username,password
 from json_handler import handler
 import cymysql, json
-conn = cymysql.connect(host=host, user=username, passwd=password, db='srchdb', charset='utf8')
 
 
-def get_json():
+def get_json(database='srchdb',table='dokarrs'):
+    conn = cymysql.connect(host=host, user=username, passwd=password, db=database, charset='utf8')
     cur = conn.cursor()
 
     #First query returning column names
     #Added to List
     columns=[]
-    cur.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = 'srchdb' AND TABLE_NAME = 'dokarrs'")
+    cur.execute("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = '%s' AND TABLE_NAME = '%s'" % (database,table))
     for r in cur.fetchall():
         columns.append(r[0])
 
 
     #Retrieve Data
     data = []
-    cur.execute('select * from dokarrs')
+    cur.execute('select * from %s' % (table))
     for r in cur.fetchall():
         #zip takes 2 list and creates tuples and dict to get dictionary
         data.append(dict(zip(columns,list(r))))
